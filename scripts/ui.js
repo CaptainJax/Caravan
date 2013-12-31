070ad33806a82d8ab185abed42185537354ab262
rabockholt@gmail.com <rabockholt@gmail.com>
2013-12-31 14:55:16 -0700

    Adding UI handlers.

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,9 +1,17 @@
 
 // User interface functions.
 
+// Initialize the UI event bindings.
+$(window).load(function() { // All assets must be loaded.
+});
+
+
 var UI = {
 
-    updateShipStatus: function() {
+    takeExit: function(e) { // Handle click on an exit.
+    }
+
+    updateShipStatus: function() { // Make display reflect the ship.
         console.log('ship at ' + ship.worldNum);
         var w = Map.getWorld(ship.worldNum);
         for (var en = 0; en < parms.world.maxExits; en++) {
